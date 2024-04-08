181c72d5bbff2d63b9a3d0776214a2a53ca0be48
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-04-08 23:08:22 +0600

    object

---
+++ ObjectObjectObject/object.js
@@ -0,0 +1,24 @@
+// create a object
+
+let user = new Object(); // object constructor syntax
+
+// or 
+
+let user1 = {}; // object literal 
+
+// Literals and properties 
+
+let users = { 
+    name : 'Jhons',
+    age : 30
+};
+// setting a object 
+users.isAdmin = true;
+
+console.log(users);
+
+
+
+
+
+
