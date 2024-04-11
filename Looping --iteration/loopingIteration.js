5cef30aef52654fbf8c572305b464fb0fd490e42
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-04-12 01:01:15 +0600

    Loops and iterations

--- Looping --iteration/loopingIteration.js
+++ Looping --iteration/loopingIteration.js
@@ -21,3 +21,51 @@ do{
     i++
 }
 while(i<age.length)
+
+
+
+// looping on object : for-in
+
+
+const person = {
+    age :12,
+    name : 21
+}
+
+for(keys in person){
+    console.log(keys); 
+
+    console.log(`${keys} : ${person[keys]}`);
+}
+
+// looping on array using for of
+
+let friends = [];
+for(friend of friends){
+    console.log(friend);
+}
+
+
+// breaks and continue : 
+
+    const simpleNumbers = [1,2,4,5,8,9,100,34,72,61,55];
+
+    console.log(simpleNumbers);
+
+    for(number of simpleNumbers){
+        console.log(number);
+        if(number===9){
+            break; // break : knows brake the loop
+            
+        }
+
+    }
+
+    for(let i =0;i<10;i++){
+        if(i===4){
+            continue;
+        }
+        console.log(i); // continue jumps 
+    }
+
+
