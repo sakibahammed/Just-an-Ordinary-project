612c348090b0c3385bc15ba25cb49a3a76bde8c8
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-04-13 12:37:35 +0600

    conditional statement

---
+++ conditionalStatement.js/condition.js
@@ -0,0 +1,37 @@
+// if else else if
+
+let happyNumber = 10;
+if(happyNumber<5){
+    console.log('Happy happy happy');
+}
+else if(happyNumber===10){
+    console.log('aaaao');
+}
+
+else{
+    console.log('Not aaao aaao aaao');
+}
+
+//switch
+
+let color = 'red';
+
+switch(color){
+    case 'blue':
+        console.log('Blue');
+    break;
+
+    case 'green' :
+        console.log('green');
+    break;
+
+    case 'white':
+        console.log('white');
+    break;
+
+    case 'red' :
+        console.log('red');
+
+    default :
+        console.log('no color');
+}
