d63acfecef90c05cf916f47cb3dbc1801ae04f69
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-04-16 17:18:24 +0600

    BigInt , String , Ternary operator or conditional operator

--- Operator/bitwise-operator.js
+++ Operator/bitwise-operator.js
@@ -52,3 +52,63 @@ console.log(5 & 0);
 
 
     console.log(5 ^ 3);
+
+
+
+// NOT (~) Bit wise NOT operation (~)
+
+// ~5 = -6
+
+// Left Shift (<<) Right Shift (>>)
+
+
+ // Left shift . shift the bits of a number to the left or right by a specified number of position.
+
+
+console.log(5<<1);
+
+console.log(5>>1);
+
+
+
+    
+
+
+
+// BigInt Operator 
+
+
+const a = 1n + 2n
+console.log(a);
+
+
+const c = 3000000000000000000n >> 2n;
+
+console.log(c);
+
+
+
+const d = 8n >>> 2n;
+
+console.log(d);
+
+
+
+const ab = Number(1n)+2;
+
+const ba = 1n + BigInt(2);
+
+
+
+// String operator
+console.log("my " + "string") ;
+
+console.log('hello'+ ' mom');
+
+
+// Conditional Operator or Ternary Operator 
+
+
+    // conditon ? val : val1
+
+const vote = age > 18?"you can vote" : "You can vote but you have to do politics";
