5c12eba69d81db0225a126dce99b755d5d790d11
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-04-16 15:35:52 +0600

    AND OR operator

--- Operator/bitwise-operator.js
+++ Operator/bitwise-operator.js
@@ -1,3 +1,43 @@
 // bit wise operator and usage
 
+
+//AND (&) operator :
+
 console.log(5 & 3);
+
+console.log(5 & 0);
+
+
+/*
+        Binary breakdown of bit wise AND operator :
+         
+        5 = 101
+        3 = 011
+
+        == 001 = 1
+
+    
+        This operator performs a bitwise AND operation on two numbers. It compares each bit of the two numbers and produces a result where the corresponding bits are set to 1 if and only if both bits are 1.
+
+        like AND operation .  1+1 = 1 , 1 + 0 = 0 , 0+0 = 0
+
+
+
+*/
+
+// OR operator : 
+
+    console.log(5|3);
+
+    // Binary breakdown is same like  Bitwise OR operator 
+
+    /* 
+
+    like 1+1 = 1 , 1+0 = 1 , 0+1 = 1, 0+0 = 0
+    
+        5 = 101 , 3 = 011 = 111 = 7
+     */
+
+
+
+
