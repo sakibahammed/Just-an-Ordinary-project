afc5cf96217d6e523a20764310c4cc803e170e1b
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-04-27 04:34:06 +0600

    what the heckk is this

---
+++ Asynchronus-Javascript/callbackHell.js
@@ -0,0 +1,31 @@
+// How the callback hell looks like
+
+
+function asyncOperation1(callback){
+    setTimeout(()=>{
+        callback('result1')
+    },1000)
+}
+
+
+function asyncOperation2(result1 , callback){
+    setTimeout(()=>{
+        callback('result2')
+    },1000)
+}
+
+function asyncOperation3(result2,callback){
+    setTimeout(()=>{
+        callback('result3')
+    },1000)
+}
+
+
+
+asyncOperation1((result1)=>{
+    asyncOperation2(result1 , (result2)=>{
+        asyncOperation3(result2,(result3)=>{
+            console.log('Final Result',result3);
+        })
+    })
+})
