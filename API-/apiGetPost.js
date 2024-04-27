53f21e374ec02f158ba0c9da32aa659d11b45c7d
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-04-28 02:40:41 +0600

    making get api

--- API-/apiGetPost.js
+++ API-/apiGetPost.js
@@ -21,3 +21,18 @@ const apiUrl = `https://jsonplaceholder.typicode.com/todos/1`;
 fetchData(apiUrl)
 .then(data=>console.log(data))
 .then(err=>console.error(err))
+
+/* 
+    fetch(url) : makes a GET request to the specified URL
+
+    await : is used to wait for the response to come back
+
+    response.ok : checks if the response was successful.
+
+    response.json() : the response is parse as JSON using response .json()
+
+    if an error occurs the catch block handles it.
+
+    
+
+*/
