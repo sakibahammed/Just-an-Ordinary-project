d8cf19c3a989c607715f702bb7c9f8b85e469f59
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-05-02 06:57:50 +0600

    strict.js

---
+++ strict-mode-js/strict.js
@@ -0,0 +1,19 @@
+// how the heck we should use strict js 
+
+"use strict";
+
+
+// strict mode in function
+
+
+function helloMom(){
+    "use strict"; // enable strict mode just placing "use strict at the top of script";
+
+    
+
+
+    console.log('hello mom');
+}
+
+
+helloMom()
