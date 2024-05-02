faa571d4cb853445cf61898868aa1fcbacbaeb5b
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-05-03 04:30:32 +0600

    basic of this keyword

---
+++ Javascript/this-keyword/thisKeyword.js
@@ -0,0 +1,63 @@
+// this in a method :
+
+/*
+        when used in an object method,`this` refers to the object. 
+
+    */
+
+const person = {
+  firstName: "sakib",
+  lastName: "ahammed",
+
+  fullName: function () {
+    return this.firstName + " " + this.lastName; // here this method refer to the person object.
+  },
+};
+
+console.log(person.fullName());
+console.log(person["firstName"]);
+
+// this Alone
+
+/* When used alone this refers to the global object because this is running in the global scope */
+
+let x = this;
+
+console.log(x);
+
+// this in a function (default)
+
+/*
+        In a function , the global object is the default binding for this.
+
+
+*/
+
+function myFunction() {
+  return this;
+}
+
+myFunction();
+
+
+
+//==> `this` in strict mode function :
+
+   /*
+    Javascript strict mode does not allow default binding. So when used in a function , in strict mode this is undefined.
+
+   */ 
+
+
+    "use strict";
+
+    function myStrictFunction(){
+        return this; // it will return undefined as strict mode doesn't allow default binding.
+    }
+
+    myStrictFunction();
+
+
+
+
+
--- strict-mode-js/strict.js
+++ strict-mode-js/strict.js
@@ -1,41 +1,30 @@
-// how the heck we should use strict js 
+// how the heck we should use strict js
 
 "use strict";
 
-
 // strict mode in function
 
+function helloMom() {
+  "use strict"; // enable strict mode just placing "use strict at the top of script";
 
-function helloMom(){
-    "use strict"; // enable strict mode just placing "use strict at the top of script";
-
-
-
-
-    console.log('hello mom');
+  console.log("hello mom");
 }
 
+helloMom();
 
-helloMom()
+// lets review some of the rules of strict mode with code example to check out :
 
-// lets review some of the rules of strict mode with code example to check out : 
+//=====> not using variables didn't created :
 
-
-//=====> not using variables didn't created : 
-
-// like this : 
-
+// like this :
 
 // x = 5;
 
 // Normally we can do this or use this but with Strict mode it ain't gonna work
 
-
-
 // console.log(x);
 
-
-// with 'use strict' we need to fix this thing like this , 
+// with 'use strict' we need to fix this thing like this ,
 
 /*
 
@@ -48,87 +37,66 @@ helloMom()
 
 */
 
+//====> not  Using the wrong `this` ;
 
-//====> not  Using the wrong `this` ; 
-
-console.log(this); // it will print 
+console.log(this); // it will print
 
 /*
     in a function , `this` might mean different things. But with strict mode , if using outside a function, `this` will be undefined
 
 */
 
+//===> not Using bad syntax :
 
-
-
-//===> not Using bad syntax : 
-
-    /*
+/*
         If try to write code in a way that's not allowed , Strict mode will stop the code.. 
     
     
     */
 
+// with(obj){
 
-    // with(obj){
+// }
 
-    // }
+// ===> Be careful with the  `argument` object:
 
+function myNumber(a, b) {
+  console.log(arguments);
+}
 
-// ===> Be careful with the  `argument` object: 
+myNumber(1, 2);
 
-    function myNumber(a,b){
-        console.log(arguments);
-
-    }
-
-    myNumber(1,2)
-
-    /*
+/*
         In strict mode , the `argument` object doesn't update automatically if you change a variable in the function. 
     
     
     */
 
-// No duplicate properties in an object : 
+// No duplicate properties in an object :
 
-    // in an object each key should be unique. Without the strict mode, it can work but with strict mode it won't gonna work.
+// in an object each key should be unique. Without the strict mode, it can work but with strict mode it won't gonna work.
 
 let person = {
-    name:'sakib',
-    age : 22,
-    age : 11,
-}
+  name: "sakib",
+  age: 22,
+  age: 11,
+};
 // multiple property wont gonna work here
 
-
-
-//Be careful with function parameters : 
+//Be careful with function parameters :
 
 // In strict mode , cann't name a function parameter the same as another one . Without strict mode it will work but as there is strict mode it gonna work..
 
-
-function myFunction(a,a){
-    console.log(a,a);
+function myFunction(a, a) {
+  console.log(a, a);
 }
 
-
-
-//=====>>>. No octal literals : in normal case use of octal is allowed but in strict mode its not allowed 
-
-
+//=====>>>. No octal literals : in normal case use of octal is allowed but in strict mode its not allowed
 
 // const myNumber = 010;
 
+//===>>> Reserved words :
 
-//===>>> Reserved words : 
+// certain words are reserved for future use in js. so dont think about it in strict mode .
 
-    // certain words are reserved for future use in js. so dont think about it in strict mode .
-
-
-    // let class = 5; // this will give you an error because class is a reserved word.
-
-
-
-
-
+// let class = 5; // this will give you an error because class is a reserved word.
