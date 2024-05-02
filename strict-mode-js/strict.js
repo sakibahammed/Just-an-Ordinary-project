bc6d97c788cdf35d46ffa2e1e69c130c0f8ae544
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-05-02 08:13:00 +0600

    strict mode in js

--- strict-mode-js/strict.js
+++ strict-mode-js/strict.js
@@ -9,7 +9,7 @@
 function helloMom(){
     "use strict"; // enable strict mode just placing "use strict at the top of script";
 
-    
+
 
 
     console.log('hello mom');
@@ -17,3 +17,98 @@ function helloMom(){
 
 
 helloMom()
+
+// lets review some of the rules of strict mode with code example to check out : 
+
+
+//=====> not using variables didn't created : 
+
+// like this : 
+
+
+// x = 5;
+
+// Normally we can do this or use this but with Strict mode it ain't gonna work
+
+
+
+// console.log(x);
+
+
+// with 'use strict' we need to fix this thing like this , 
+
+/*
+
+    "use strict";
+
+    let x = 5;
+
+    console.log(x);
+
+
+*/
+
+
+//====> not  Using the wrong `this` ; 
+
+console.log(this); // it will print 
+
+/*
+    in a function , `this` might mean different things. But with strict mode , if using outside a function, `this` will be undefined
+
+*/
+
+
+
+
+//===> not Using bad syntax : 
+
+    /*
+        If try to write code in a way that's not allowed , Strict mode will stop the code.. 
+    
+    
+    */
+
+
+    // with(obj){
+
+    // }
+
+
+// ===> Be careful with the  `argument` object: 
+
+    function myNumber(a,b){
+        console.log(arguments);
+
+    }
+
+    myNumber(1,2)
+
+    /*
+        In strict mode , the `argument` object doesn't update automatically if you change a variable in the function. 
+    
+    
+    */
+
+// No duplicate properties in an object : 
+
+    // in an object each key should be unique. Without the strict mode, it can work but with strict mode it won't gonna work.
+
+let person = {
+    name:'sakib',
+    age : 22,
+    age : 11,
+}
+// multiple property wont gonna work here
+
+
+
+//Be careful with function parameters : 
+
+// In strict mode , cann't name a function parameter the same as another one . Without strict mode it will work but as there is strict mode it gonna work..
+
+
+function myFunction(a,a){
+    console.log(a,a);
+}
+
