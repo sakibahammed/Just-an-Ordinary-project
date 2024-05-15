bd67adf97df227b73cee4182f2ef036ade6170e1
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-05-15 23:42:55 +0600

    iteratorjs

--- Iterator-Generator/iterator.js
+++ Iterator-Generator/iterator.js
@@ -1,3 +1,11 @@
 console.log('hello mom');
 
 // what the heck is iterator any way?
+
+
+let items = ['keyboard' , 'mouse' , 'speaker','loud machine noise'];
+
+let dispenser = items[Symbol.iterator]();
+
+
+console.log(dispenser.next());
