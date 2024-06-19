773d821710a3b799c35f811e4749e973707de565
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-20 01:05:06 +0600

    # allocation in js , value initilization

--- Memory-Management/memory.js
+++ Memory-Management/memory.js
@@ -1,2 +1,36 @@
-const n = 23 ; 
-const s = "aserty"
+// Allocation in Javascript
+
+/*
+    In order to not bother fellow programmer with allocation , JS will automatically allocate memory when  values are initially declared. 
+
+*/
+
+// Value initialization 
+
+const n  = 123; // allocates  memory for a number
+
+const s = "azerty" ; // allocates memory for a string . 
+
+const o = {
+    a : 1,
+    b : null
+} // allocates memory for an object and contained values . 
+
+// (like object) allocates  memory for the array and contained values . 
+
+const  a = [1, null , 'abra'];
+
+function f(a){
+    return a+1;
+} // allocate a function which is a callabale object
+
+// function expression also allocate an object
+
+SVGGeometryElement.addEventListener(
+    'click',()=>{
+        SVGGeometryElement.style.background = 'blue';
+    },
+    false,
+);
+
+
